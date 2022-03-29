aa0da50ca5747337e33f94ead9b9b6f2bbafedc3
Axaerus <rgater08@gmail.com>
2022-03-29 14:20:37 +0100

    Added flag set for level loading
    Added setLoaded to level1 loading function to notify of the level being loaded
    Commented out faking loading time function to load level immediately
    Set Render function to render level when level is loaded

--- NbrhdNash/Scenes/scene_Level1.cpp
+++ NbrhdNash/Scenes/scene_Level1.cpp
@@ -5,6 +5,14 @@
 void Level1::Load() {
 	//ls::loadLevelFile("res/levels/level1.txt", 50.f);
 	ls::loadLevelFile("res/levels/TileTestLevel.txt", 50.f);
+
+	auto ho = Engine::getWindowSize().y - (ls::getHeight() * 40.f);
+	ls::setOffset(sf::Vector2f(0, ho));
+
+	//std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+	std::cout << " Scene 1 Load Done" << std::endl;
+
+	setLoaded(true);
 }
 
 void Level1::UnLoad() {
@@ -12,6 +20,7 @@ void Level1::UnLoad() {
 }
 
 void Level1::Render() {
+	ls::render(Engine::GetWindow());
 	Scene::Render();
 }
 
