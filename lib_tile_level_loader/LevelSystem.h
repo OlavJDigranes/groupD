7eaaf9e7a3bed3d37c07ed24521fbfa029581941
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-03-29 15:04:21 +0100

    Comment cleanup
    Removed block comments of old code, and added explanatory comments for Tiles and colours.

--- lib_tile_level_loader/LevelSystem.h
+++ lib_tile_level_loader/LevelSystem.h
@@ -16,6 +16,7 @@ public:
 
     typedef unsigned char Tile;
 
+    //Tile enumeration using legend from shared document
     enum TILES {
         EMPTY = ' ',
         EDGEWALL = 'w',
@@ -84,54 +85,3 @@ private:
     ~LevelSystem() = delete;
 };
 
-/*
-class LevelSystem {
-public:
-    enum TILE { EMPTY, START, END, WALL, ENEMY, WAYPOINT };
-
-    //Get Width
-    static size_t getWidth() {
-        return _width;
-    };
-    //Get Height
-    static size_t getHeight() {
-        return _height;
-    };
-
-    static void loadLevelFile(const std::string&, float tileSize = 100.f);
-    static void Render(sf::RenderWindow& window);
-    static sf::Color getColor(TILE t);
-    static void setColor(TILE t, sf::Color c);
-    //Get Tile at grid coordinate
-    static TILE getTile(sf::Vector2ul);
-    static std::vector<sf::Vector2ul> findTiles(TILE Tile);
-    //Get Screenspace coordinate of tile
-    static sf::Vector2f getTilePosition(sf::Vector2ul);
-    //get the tile at screenspace pos
-    static TILE getTileAt(sf::Vector2f);
-    static sf::Vector2f getStartTilePosition();
-    static sf::Vector2f getEndTilePosition();
-
-protected:
-    static std::unique_ptr<TILE[]> _tiles; //Internal array of tiles
-    static size_t _width; //how many tiles wide is level
-    static size_t _height; //how many tile high is level
-    static sf::Vector2f _offset; //Screenspace offset of level, when rendered.
-    static float _tileSize; //Screenspace size of each tile, when rendered.
-    static std::map<TILE, sf::Color> _colours; //color to render each tile type
-
-    //array of sfml sprites of each tile
-    static std::vector<std::unique_ptr<sf::RectangleShape>> _sprites;
-
-    //Start and end positions for game logic
-    static std::shared_ptr<sf::Vector2f> _startPos;
-    static std::shared_ptr<sf::Vector2f> _endPos;
-
-    //generate the _sprites array
-    static void buildSprites();
-
-private:
-    LevelSystem() = delete;
-    ~LevelSystem() = delete;
-};
-*/
