e5bd468dc3eb4ac1c7014d1a123578e48c09418a
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-04-19 17:47:14 +0100

    LevelTimer implemented
    added and implemented the level timer component for use in game levels. Subject to testing and level implementation.

---
+++ lib_ecm/components/cmp_timer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <../lib_ecm/ecm.h>
+#include <engine.h>
+#include <fstream>
+
+using namespace std;
+using namespace sf; 
+
+class LevelTimer : public Component {
+protected:
+	//Timer
+	sf::Clock timer; 
+	int tempLevelTag; 
+	float timeInSeconds; 
+	ofstream timeFile; 
+public: 
+	LevelTimer() = delete;
+
+	explicit LevelTimer(Entity* const p, int levelTag);
+
+	~LevelTimer() override = default;
+	void LevelTimerStop();
+	//void update(double dt) override; 
+	//void render() override; 
+};
