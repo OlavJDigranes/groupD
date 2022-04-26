7af7f89b67614ab8e91f6cd35e190bb6876e7883
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-04-26 23:52:18 +0100

    Attempted implementation of engine sound EOD
    Attempted implementation of engine sound. The sound is loaded properly but it does not play when the boolean is set to true, possibly due to a logic error in the application of the if statements checking if the bool is set to true.

--- lib_ecm/components/cmp_player_controller.cpp
+++ lib_ecm/components/cmp_player_controller.cpp
@@ -10,7 +10,12 @@ PlayerController::PlayerController(Entity* p, std::weak_ptr<DrivingComponent> dr
             std::cout << "Joystick " << i << " is connected!" << std::endl;
         else
             std::cout << "Joystick " << i << " is NOT connected!" << std::endl;
-    }
+    }    
+
+    //Driving sound
+    carEngineBuffer.loadFromFile("res/music/CarSound.mp3");
+    carEngine.setBuffer(carEngineBuffer);
+    carEngine.setVolume(90);
 }
 
 PlayerController::~PlayerController() {
@@ -28,9 +33,12 @@ void PlayerController::update(double dt) {
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
             d->Drive(1, dt);   // Drive forwards
+            //Driving sound
+            soundOn = true;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
             d->Brake(dt);  // Brake
+            soundOn = false;
         }
 
         sf::Joystick::Identification joystickID = sf::Joystick::getIdentification(0);
@@ -38,21 +46,28 @@ void PlayerController::update(double dt) {
             float joystickXYAxisPos = Joystick::getAxisPosition(0, sf::Joystick::Axis::X);
             float joystickTAxisPos = Joystick::getAxisPosition(0, sf::Joystick::Axis::Z);
             if (joystickXYAxisPos < -50.0f) {
-                cout << "L" << endl; 
                 d->Rotate(-180, dt);   // Turn left
             }
             if (joystickXYAxisPos > 50.0f) {
-                cout << "R" << endl;
                 d->Rotate(180, dt);    // Turn right
             }
             if (joystickTAxisPos < 0) {
-                cout << "D" << endl;
                 d->Drive(1, dt);   // Drive forwards
             }
             if (joystickTAxisPos > 0) {
-                cout << "R" << endl;
                 d->Brake(dt);  // Brake
             }
+        }
+
+        //Car sounds
+        if (soundOn) {
+            std::cout << "wg" << std::endl;
+            carEngine.play();
+            //carEngine.setLoop(true);
+        }
+        if (!soundOn) {
+            std::cout << "rh" << std::endl;
+            carEngine.stop();
         }
 	}
 	else {
