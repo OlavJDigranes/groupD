3eb5a41cfaadcb85539bcb77d437ab53cfff1997
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-08-08 17:24:42 +0100

    Update scene_VisualSettings.cpp

--- NbrhdNash/Scenes/scene_VisualSettings.cpp
+++ NbrhdNash/Scenes/scene_VisualSettings.cpp
@@ -35,6 +35,8 @@ void VisualSettings::Load() {
 	auto u = titleText->addComponent<TextComponent>("Visual Settings");
 	float txtOffset = 0.0f;
 
+	/*
+	* NOTES: These are the implemented settigns options. They need to be uniform with rest of menus in how to chose them. 
 	auto info2 = makeEntity();
 	info2->setPosition(Vector2f(Engine::getWindowSize().x * 0.3, Engine::getWindowSize().y * 0.2));
 	if (Joystick::isConnected(0)) {
@@ -43,6 +45,7 @@ void VisualSettings::Load() {
 	else {
 		auto i2 = info2->addComponent<TextComponent>("SELECT RESOLUTION:\nQ: 1280 x 720\nW: 1920 x 1080\nE: 2560 x 1440\n\nV-SYNC:\nV: On\nB: Off");
 	}
+	*/
 
 	setLoaded(true);
 }
